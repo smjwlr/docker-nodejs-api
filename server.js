3f8d8612e706d6853086ec26bb36e6d28c16c13f
Sam Weller <smjwlr@gmail.com>
2023-07-05 07:13:07 -0700

    .

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
   res.send('Well hello there');
 });
 
+app.get('/hey', (req, res) => {
+  res.send('Oh hey');
+});
+
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
